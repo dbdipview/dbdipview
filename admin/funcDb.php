5d3a96b8897c585daf6897b4834a72bb0d37bfca
BorisD <boris.domajnko@gov.si>
2019-09-06 12:09:35 +0200

    more options for menu scenarios

--- admin/funcDb.php
+++ admin/funcDb.php
@@ -5,9 +5,22 @@
  */
  
  
+
+ /**
+ *  List all available databases, then exit.
+ *
+ */
+ function dbf_list_databases() {
+	global $PGPASSWORD, $DBADMINUSER;
+	
+	passthru("PGPASSWORD=$PGPASSWORD psql -P pager=off -l -U $DBADMINUSER");
+ }
+ 
+ 
 /**
  * Create a database container
- *
+ * This is a PostgreSQL database. 
+ * Into it, one or more schemas may be installed if they do not interfere one with another 
  */
 function dbf_create_dbc($DBC) {
 	global $MSG32_SERVER_DATABASE_NOT_SELECTED, $MSG11_DB_ALREADY_EXISTS, $MSG22_DB_CREATED;
@@ -58,9 +71,121 @@ function dbf_delete_dbc($DBC) {
 	else {
 		msgCyan($MSG26_DELETING . ": " . $DBC);
 		passthru("PGPASSWORD=$PGPASSWORD dropdb " . $DBC . 
-				" -U ". $DBADMINUSER . " --if-exists", $rv);
+			" -U ". $DBADMINUSER . " --if-exists", $rv);
 		$retval = $OK;
 	}
+	return($retval);
 }
 
 
+ /**
+ *  Create schema 
+ *
+ */
+ function  dbf_create_schema($DBC, $SCHEMA) {
+	global $PGPASSWORD, $DBADMINUSER;
+	
+	$rv = "";
+	passthru("echo CREATE SCHEMA " . $SCHEMA . " AUTHORIZATION " . $DBADMINUSER . 
+		"| PGPASSWORD=$PGPASSWORD psql " . $DBC . " -U " . $DBADMINUSER, $rv);
+	return($rv);
+ }
+ 
+  /**
+ *  Grant usage on schema 
+ *
+ */
+ function  dbf_grant_usage_on_schema($DBC, $SCHEMA, $DBGUEST) {
+	global $PGPASSWORD, $DBADMINUSER;
+	
+	$rv = "";
+	passthru("echo GRANT USAGE ON SCHEMA " . $SCHEMA . " TO " . $DBGUEST . 
+		"| PGPASSWORD=$PGPASSWORD psql " . $DBC . " -U " . $DBADMINUSER, $rv);
+	return($rv);
+ }
+
+ /**
+ *  Drop schema 
+ *
+ */
+ function  dbf_drop_schema($DBC, $SCHEMA) {
+	global $PGPASSWORD, $DBADMINUSER;
+	
+	$rv = "";
+	passthru("echo DROP SCHEMA " . $SCHEMA . 
+		" CASCADE | PGPASSWORD=$PGPASSWORD psql " . $DBC . " -U " . $DBADMINUSER, $rv);
+	return($rv);
+ }
+ 
+ 
+  /**
+ *  Grant select on table
+ *
+ */
+ function dbf_grant_select_on_table($DBC, $TABLE, $DBGUEST) {
+	global $PGPASSWORD, $DBADMINUSER;
+	
+	$rv = "";
+	passthru("echo GRANT SELECT ON " . $TABLE . " TO " . $DBGUEST . 
+		"| PGPASSWORD=$PGPASSWORD psql " . $DBC . " -U " . $DBADMINUSER);
+	return($rv);
+ }
+									
+ /**
+ *  Grant select on all tables in schema for the user 
+ *
+ */
+ function dbf_grant_select_all_tables($DBC, $SCHEMA, $DBGUEST) {
+	global $PGPASSWORD, $DBADMINUSER;
+	
+	$rv = "";
+	passthru("echo GRANT USAGE ON SCHEMA " . $SCHEMA . " TO " . $DBGUEST . 
+		"| PGPASSWORD=$PGPASSWORD psql " . $DBC . " -U " . $DBADMINUSER, $rv);
+		
+	passthru("echo GRANT SELECT ON ALL TABLES IN SCHEMA " . $SCHEMA . " TO " . $DBGUEST . 
+		"| PGPASSWORD=$PGPASSWORD psql " . $DBC . " -U " . $DBADMINUSER, $rv);
+	return($rv);
+ }
+
+ /**
+ *  Load data into table from CSV delimited file
+ *
+ */
+ function dbf_populate_table_csv($DBC, $DATEMODE, $TABLE, $SRCFILE, $DELIMITER, $HEADER) {
+	global $PGPASSWORD, $DBADMINUSER;
+	
+	$rv = ""; 
+	passthru("echo SET datestyle=" . $DATEMODE . "\;" . 
+		"COPY " . $TABLE . " FROM \'$SRCFILE\' DELIMITER E\'$DELIMITER\' CSV $HEADER" . 
+		" | PGPASSWORD=$PGPASSWORD psql " . $DBC . " -U " . $DBADMINUSER);
+	return($rv);
+ }
+
+ /**
+ *  Load data into table from TAB delimited file
+ *
+ */
+ function dbf_populate_table_tab($DBC, $DATEMODE, $TABLE, $SRCFILE, $DELIMITER, $HEADER) {
+	global $PGPASSWORD, $DBADMINUSER;
+	
+	$rv = ""; 
+	passthru("echo SET datestyle=" . $DATEMODE . "\;" . 
+		"COPY " . $TABLE . " FROM \'$SRCFILE\' DELIMITER E\'$DELIMITER\' $HEADER WITH NULL AS \'\'" . 
+		" | PGPASSWORD=$PGPASSWORD psql " . $DBC . " -U " . $DBADMINUSER);
+	return($rv);
+ }
+ 
+										
+ /**
+ *  Run script 
+ *
+ */
+ function  dbf_run_sql($DBC, $SQL) {
+	global $PGPASSWORD, $DBADMINUSER; 
+	
+	$rv = "";
+	passthru("cat ".$SQL."| PGPASSWORD=$PGPASSWORD psql " . $DBC . " -U " . $DBADMINUSER, $rv);
+	return($rv);
+ }
+ 
+	
